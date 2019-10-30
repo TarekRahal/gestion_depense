41e08260fbc972c37773f0bbea4397784bb1c1b8
SalmaAlaoui <51100352+SalmaAlaoui@users.noreply.github.com>
2019-10-30 16:19:17 +0100

    test test allah allah 2

---
+++ mobileapp/src/main/java/com/example/mobileapp/Models/Salaire.java
@@ -0,0 +1,77 @@
+package com.example.mobileapp.Models;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.SequenceGenerator;
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+@Entity
+
+public class Salaire implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "credit_seq")
+    @SequenceGenerator(name = "credit_seq", sequenceName = "credit_seq", allocationSize = 1)
+    private Long id;
+    private String travail;
+    @ManyToOne
+    @JoinColumn(name = "revenuId")
+    @JsonBackReference
+    private Revenu revenu;
+
+
+    /**
+     * @return Long return the id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return Revenu return the revenu
+     */
+    public Revenu getRevenu() {
+        return revenu;
+    }
+
+    /**
+     * @param revenu the revenu to set
+     */
+    public void setRevenu(Revenu revenu) {
+        this.revenu = revenu;
+    }
+
+
+    /**
+     * @return String return the travail
+     */
+    public String getTravail() {
+        return travail;
+    }
+
+    /**
+     * @param travail the travail to set
+     */
+    public void setTravail(String travail) {
+        this.travail = travail;
+    }
+
+}
